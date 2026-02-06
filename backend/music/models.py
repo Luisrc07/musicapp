40e069faebf9eb456579b3e4a70efbf7d4a4395f
Luisrc07 <luisrodrgzc@gmail.com>
2026-02-05 22:56:22 -0400

    back actualizado

--- backend/music/models.py
+++ backend/music/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Artist Name")
+    bio= models.TextField(blank=True, verbose_name="Biography")
+    image= models.ImageField(upload_to='artists/', blank=True, null=True, verbose_name="Artist Image")
+
+    class Meta():
+        verbose_name = "Artist"
+        verbose_name_plural = "Artists"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums', verbose_name="Artist")
+    title = models.CharField(max_length=100, verbose_name="Album Title")
+    cover_image = models.ImageField(upload_to='covers/', blank=True, null=True, verbose_name="Album Cover Image")
+    release_date = models.DateField(verbose_name="Release Date", auto_now_add=True)
+    
+
+    class Meta():
+        verbose_name = "Album"
+        verbose_name_plural = "Albums"
+        ordering = ['title']
+        
+    def __str__(self):
+        return f"{self.title} {self.artist.name}" 
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='songs', verbose_name="Album")
+    tittle = models.CharField(max_length=100, verbose_name="Song Title")
+    audio_file = models.FileField(upload_to='songs/', verbose_name="Audio File")
+    duration = models.DurationField(verbose_name="Duration")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+
+    class Meta():
+        verbose_name = "Song"
+        verbose_name_plural = "Songs"
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.tittle
--- backend/music/views.py
+++ backend/music/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Artist, Album, Song
+from .serializers import ArtistSerializer, AlbumSerializer, SongSerializer
 
-# Create your views here.
+class ArtistViewSet(viewsets.ModelViewSet):
+    """
+    Lógica para manejar Artistas
+    """
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    """
+    Lógica para manejar Álbumes
+    """
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class SongViewSet(viewsets.ModelViewSet):
+    """
+    Lógica para manejar Canciones
+    """
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
